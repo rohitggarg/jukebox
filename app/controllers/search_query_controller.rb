dae3ea8d247ba748cb1a73dde7fa98438abf1099
RohitG <rohitgarg19@gmail.com>
2017-12-22 23:21:57 +0530

    Fixing search result error caused due to ads

--- app/controllers/search_query_controller.rb
+++ app/controllers/search_query_controller.rb
@@ -13,6 +13,6 @@ class SearchQueryController < ApplicationController
     response = JSON.parse(Net::HTTP.get(uri))
     body_part = response.last["body"]
     search_results = body_part["content"].scan(/\<h3.*?\<a href.*?\<\/h3\>/)
-    search_results.collect {|a| { name: a.match(/title=".*?"/)[0].gsub("title=","").gsub("\"",""), url: a.match(/href="\/.*?"/)[0].gsub("href=","").gsub("\"","") }}
+    search_results.select {|a| a.match(/href=\"\/.*?"/) != nil }.collect {|a| { name: a.match(/title=".*?"/)[0].gsub("title=","").gsub('"',""), url: a.match(/href="\/.*?"/)[0].gsub("href=","").gsub('"',"") }}
   end
 end
